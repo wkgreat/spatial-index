2a59ab53a73c5915556f3b22523d0909cbef29f9
ke.wang <ke.wang@transwarp.io>
2025-08-27 10:50:38 +0800

    build: webpack and basic framework

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js', // 入口文件
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'development', // 或 'production'
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        port: 9001,
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/, // 处理 CSS 文件
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+    ]
+};

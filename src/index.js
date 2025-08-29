6fb1b34549507f621e9f38feded0e4f9a6edf6d5
ke.wang <ke.wang@transwarp.io>
2025-08-29 11:33:24 +0800

    ci: github page

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ async function main() {
     const rtree = new RTree(2, 5);
     const render = new RTreeRender(rtree);
     const [xmin, ymin, xmax, ymax] = [0, 0, 10, 10];
+
+    for (let i = 0; i < 20; i++) {
+        rtree.insert(Geometry.buildRandom(xmin, ymin, xmax, ymax));
+    }
+    render.render();
+
     document.getElementById("the-button").addEventListener("click", () => {
         rtree.insert(Geometry.buildRandom(xmin, ymin, xmax, ymax));
         render.render();

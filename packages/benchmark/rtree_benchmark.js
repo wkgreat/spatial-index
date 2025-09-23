115fb6b9515a03d7a4eaf8926da60d65bf8dc02e
wkgreat <wkgreat@outlook.com>
2025-09-23 10:50:08 +0800

    fix: rtree linearpickseeds

--- packages/benchmark/rtree_benchmark.js
+++ packages/benchmark/rtree_benchmark.js
@@ -13,17 +13,17 @@ const polygonToRTreeMBR = (g) => {
 }
 
 function prepareData() {
-    const worldBox = new geom.Envelope(0, 100, 0, 100);
-    const geomsize = { hmin: 2, wmin: 2, hmax: 10, wmax: 10 };
-    const polygons = randomPolygons(worldBox, 1000, geomsize);
+    const worldBox = new geom.Envelope(0, 1000, 0, 1000);
+    const geomsize = { hmin: 2, wmin: 2, hmax: 100, wmax: 100 };
+    const polygons = randomPolygons(worldBox, 1000000, geomsize);
 
-    const rtree = new RTree(5, 10);
+    const rtree = new RTree(20, 50);
     rtree.setToMbrFunc(polygonToRTreeMBR);
     for (let g of polygons) {
         rtree.insert(g);
     }
 
-    const search_size = { hmin: 20, wmin: 20, hmax: 40, wmax: 40 };
+    const search_size = { hmin: 20, wmin: 20, hmax: 50, wmax: 50 };
     const search_polygon = randomPolygon(worldBox, search_size);
 
     return {
@@ -37,17 +37,13 @@ const { polygons, rtree, search_polygon } = prepareData();
 
 suite
     .add('search brute force', () => {
-        for (let i = 0; i < 100; ++i) {
-            const search_result = polygons.filter(g => RelateOp.overlaps(g, search_polygon));
-        }
+        const search_result = polygons.filter(g => RelateOp.overlaps(g, search_polygon));
 
     })
     .add('search rtree', () => {
-        for (let i = 0; i < 100; ++i) {
-            const search_result = rtree.search_overlap(polygonToRTreeMBR(search_polygon))
-                .map(e => e.record.data)
-                .filter(g => RelateOp.overlaps(g, search_polygon));
-        }
+        const search_result = rtree.search_overlap(polygonToRTreeMBR(search_polygon))
+            .map(e => e.record.data)
+            .filter(g => RelateOp.overlaps(g, search_polygon));
 
     }).on('cycle', function (event) {
         const t = event.target;
@@ -60,13 +56,6 @@ suite
     })
     // 测试完成后输出最快的
     .on('complete', function (event) {
-        // console.log('=====complete=====');
-        // const t = event.target;
-        // console.log(`=====Name: ${t.name}=====`);
-        // console.log(`Ops/sec: ${t.hz.toFixed(2)}`);
-        // console.log(`Avg time per op: ${(t.stats.mean * 1000).toFixed(6)} ms`);
-        // console.log(`StdDev: ${(t.stats.deviation * 1000).toFixed(6)} ms`);
-        // console.log(`RME: ${t.stats.rme.toFixed(2)}%`);
-        // console.log(`Samples: ${t.stats.sample.length}`);
+        console.log('Fastest is ' + this.filter('fastest').map('name'));
     })
-    .run({ async: false });
+    .run({ async: true });
